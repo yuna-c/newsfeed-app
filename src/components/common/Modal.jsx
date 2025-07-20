41f5774cb793f91dc056ca06511534de190534a0
yuna <yuna744@gmail.com>
2025-07-21 07:04:23 +0900

    refactor : scroll-lock 라이브러리로 스크롤 제어 방식 개선 - yarn add scroll-lock - document=hidden은 모바일에서 깨지기 쉽기 때문에 모달을 열기전 까지 스크롤 내린 위치가 변경되지 않기위해 사용 - 여러개 겹쳐도 내부적으로 잠금큐(stack)을 관리 - 특정 dom영역만 막을 때 유용 - 클린업 함수로 다시 실행 전에 멈춤

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,26 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import scrollLock from 'scroll-lock';
 
 function Modal({ isOpen, onClose, children }) {
-  
+  // scroll 막을 대상을 useRef로 Dom의 어떤 요소의 스클롤을 막을지 직접 지정 console.log(scrollLock);
+  const modalRef = useRef(null);
+
   useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
+    if (isOpen && modalRef.current) {
+      scrollLock.disablePageScroll(modalRef.current);
     } else {
-      document.body.style.overflow = '';
+      scrollLock.enablePageScroll(modalRef.current);
     }
 
     return () => {
-      document.body.style.overflow = '';
+      scrollLock.clearQueueScrollLocks();
     };
   }, [isOpen]);
 
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-10 flex items-center justify-center w-full h-full bg-black/50" onClick={onClose}>
+    <div
+      className="fixed inset-0 z-10 flex items-center justify-center w-full h-full bg-black/50"
+      onClick={onClose}
+      ref={modalRef} // ← 여기를 scrollLock이 참조해서 잠금
+    >
       <div
         onClick={(e) => e.stopPropagation()} //내용 눌러도 안닫히게
-        className="p-8 bg-white rounded-lg min-w-[90%] sm:min-w-[400px] xl:w-[50%]"
+        className="p-8 bg-white rounded-lg min-w-[90%] sm:min-w-[400px] xl:w-[50%] overflow-auto max-h-[90%]"
       >
         {children}
       </div>
