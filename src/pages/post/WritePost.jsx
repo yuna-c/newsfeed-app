73f92e3e08487df4401c6c3752d6cbb11b395483
yuna <yuna744@gmail.com>
2025-07-22 03:58:00 +0900

    add : state 버전 기본 폼 데이터 전송

--- src/pages/post/WritePost.jsx
+++ src/pages/post/WritePost.jsx
@@ -1,40 +1,45 @@
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { supabase } from '../../supabase/Client';
 
 function WritePost() {
   const { user } = useContext(AuthContext);
-  const fileInputRef = useRef(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [content, setContent] = useState('');
+  const [hashtag, setHashtag] = useState('');
+  const [projectStartDate, setProjectStartDate] = useState('');
+  const [projectEndDate, setProjectEndDate] = useState('');
 
-  const [images, setImages] = useState([]); // 업로드된 이미지 URL 배열
-  const [previewUrl, setPreviewUrl] = useState([]); // 미리보기
-  const [thumbnailIndex, setThumbnailIndex] = useState(0);
-  const [uploading, setUploading] = useState(false);
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-  const navigate = useNavigate();
+    try {
+      const update = {
+        nick_name: user.nick_name,
+        title: title,
+        description: description,
+        content: content,
+        hash_tag: hashtag.split(' '),
+        project_start_date: projectStartDate,
+        project_end_date: projectEndDate
+      };
 
-  useEffect(() => {
-    console.log(`유저 정보🙋`, user);
-  }, []);
+      const { data, error } = await supabase.from('posts').insert(update).select();
 
-  const onSubmit = () => {};
-  const onChange = () => {};
-  const onChangeImage = () => {};
+      if (error) throw error;
+      console.log(data);
+    } catch (error) {
+      console.error('포스트 작성 실패', error.message);
+    }
+  };
 
-  const [formData, setFormData] = useState({
-    title: '',
-    description: '',
-    content: '',
-    projectStartDate: '',
-    projectEndDate: '',
-    hashtag: ''
-  });
   return (
     <section className="flex items-center justify-center min-h-[calc(100vh-10rem)]">
       <article className="w-full space-y-6 xl:w-1/3">
         <h2 className="text-3xl font-extrabold">글쓰기</h2>
 
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="space-y-4">
           <fieldset>
             <label htmlFor="title" className="mb-1 text-sm font-medium text-gray-700">
               제목
@@ -42,12 +47,13 @@ function WritePost() {
             <input
               type="text"
               id="title"
-              value={formData.title}
+              value={title}
               name="title"
-              onChange={onChange}
+              onChange={(e) => setTitle(e.target.value)}
               className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </fieldset>
+
           <fieldset>
             <label htmlFor="description" className="mb-1 text-sm font-medium text-gray-700">
               설명
@@ -55,9 +61,9 @@ function WritePost() {
             <input
               type="text"
               id="description"
-              value={formData.description}
+              value={description}
               name="description"
-              onChange={onChange}
+              onChange={(e) => setDescription(e.target.value)}
               className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </fieldset>
@@ -68,9 +74,9 @@ function WritePost() {
             <input
               type="text"
               id="hashtag"
-              value={formData.hashtag}
+              value={hashtag}
               name="hashtag"
-              onChange={onChange}
+              onChange={(e) => setHashtag(e.target.value)}
               className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </fieldset>
@@ -80,51 +86,45 @@ function WritePost() {
             </label>
             <textarea
               rows="10"
-              value={formData.content}
-              onChange={onChange}
+              id="content"
+              value={content}
+              name="content"
+              onChange={(e) => setContent(e.target.value)}
               className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </fieldset>
           <fieldset>
-            <label htmlFor="" className="mb-1 text-sm font-medium text-gray-700">
+            <label htmlFor="date" className="mb-1 text-sm font-medium text-gray-700">
               기간
             </label>
             <div className="flex">
               <input
                 type="date"
-                value={formData.projectStartDate}
-                onChange={onChange}
+                id="projectStartDate"
+                value={projectStartDate}
+                name="projectStartDate"
+                onChange={(e) => setProjectStartDate(e.target.value)}
                 className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
               <span className="p-2">~</span>
               <input
                 type="date"
-                value={formData.projectEndDate}
-                onChange={onChange}
+                id="projectEndDate"
+                value={projectEndDate}
+                name="projectEndDate"
+                onChange={(e) => setProjectEndDate(e.target.value)}
                 className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
               />
             </div>
           </fieldset>
-          <fieldset>
-            <label htmlFor="file" className="mb-1 text-sm font-medium text-gray-700">
-              파일선택
-            </label>
-            <input
-              type="file"
-              accept="image/*"
-              ref={fileInputRef}
-              multiple
-              onChange={onChange}
-              className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-            />
-          </fieldset>
-
-          <button
-            type="submit"
-            className="inline-flex justify-center w-1/2 px-4 py-2 font-semibold text-white rounded-md text-sm/6 bg-gray-950 hover:bg-gray-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-950/"
-          >
-            업로드중 /포스트 작성하기
-          </button>
+          <div className="flex w-full gap-2">
+            <button
+              type="submit"
+              className="inline-flex justify-center w-full px-4 py-2 font-semibold text-white rounded-md text-sm/6 bg-gray-950 hover:bg-gray-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-950/"
+            >
+              포스트 작성하기
+            </button>
+          </div>
         </form>
       </article>
     </section>
