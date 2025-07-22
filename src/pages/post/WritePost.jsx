cdd51dcf8f0f0982d8cbdfaacd387451392d2c7a
yuna <yuna744@gmail.com>
2025-07-22 14:11:01 +0900

    add : 다중 파일 storage 업로드 후, 이미지 URL 가지고 오기

--- src/pages/post/WritePost.jsx
+++ src/pages/post/WritePost.jsx
@@ -10,6 +10,7 @@ function WritePost() {
   const [hashtag, setHashtag] = useState('');
   const [projectStartDate, setProjectStartDate] = useState('');
   const [projectEndDate, setProjectEndDate] = useState('');
+  const [images, setImages] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -22,7 +23,8 @@ function WritePost() {
         content: content,
         hash_tag: hashtag.split(' '),
         project_start_date: projectStartDate,
-        project_end_date: projectEndDate
+        project_end_date: projectEndDate,
+        images: [images]
       };
 
       const { data, error } = await supabase.from('posts').insert(update).select();
@@ -32,6 +34,60 @@ function WritePost() {
     } catch (error) {
       console.error('포스트 작성 실패', error.message);
     }
+  };
+
+  /*
+  onChangeImages = async (e) => {
+  1. 파일 목록 가져오기
+  2. 각각의 파일을:
+    └ 이름 만들기 → 업로드 → URL 가져오기 → 배열에 저장
+  3. 상태 업데이트: setImages
+}
+  */
+
+  const onChangeImages = async (e) => {
+    const files = Array.from(e.target.files); // 1. 파일 목록
+    // Array.from(유사배열) **유사 배열(iterable)**을 **진짜 배열(Array)**로 바꿔주는 메서드 이렇게 해야 .map(), .forEach() 등 배열 메서드 사용 가능
+    console.log(e.target.files);
+    const uploadedUrls = [];
+    console.log(uploadedUrls);
+
+    for (const file of files) {
+      const ext = file.name.split('.').pop(); // 2. 확장자 추출
+      console.log(ext);
+
+      const filename = `${user.id}-${crypto.randomUUID()}.${ext}`; // 4. 업로드 경로
+      console.log(filename);
+
+      const filepath = `posts/${filename}`; // 4. 업로드 경로
+      console.log(filepath);
+
+      // 5. Supabase 업로드
+      const { data, error: uploadError } = await supabase.storage.from('images').upload(filepath, file);
+      console.log(`data`, data);
+      console.log(uploadError);
+
+      if (uploadError) {
+        console.error('이미지 업로드 실패', uploadError.message);
+        continue;
+      }
+
+      // 6. 공개 URL 가져오기
+      const { data: publicUrlData, error: ulrError } = supabase.storage.from('images').getPublicUrl(filepath);
+      console.log(publicUrlData, ulrError);
+
+      if (ulrError) {
+        console.error('URL 추출 실패', error.message);
+        continue;
+      }
+      // 7. 배열에 저장
+      uploadedUrls.push(publicUrlData.publicUrl);
+      console.log(uploadedUrls);
+    }
+
+    // 8. 상태 반영
+    setImages(uploadedUrls);
+    console.log(images);
   };
 
   return (
@@ -117,6 +173,19 @@ function WritePost() {
               />
             </div>
           </fieldset>
+          <fieldset>
+            <label htmlFor="file" className="mb-1 text-sm font-medium text-gray-700">
+              파일선택
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={onChangeImages}
+              className="w-full px-3 py-2 text-sm border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </fieldset>
+
           <div className="flex w-full gap-2">
             <button
               type="submit"
